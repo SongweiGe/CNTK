619d5b71a7fc5c5815e83dbc41e922f834de3c2f
Songwei Ge <songweig@vulcansub00.umiacs.umd.edu>
2020-10-05 03:38:06 -0400

    scripts

---
+++ run14.sh
@@ -0,0 +1,4 @@
+python CNTK.py --num_train 10 --depth 14 > logs/num_train_10_depth_14.txt
+python CNTK.py --num_train 40 --depth 14 > logs/num_train_40_depth_14.txt
+python CNTK.py --num_train 160 --depth 14 > logs/num_train_160_depth_14.txt
+python CNTK.py --num_train 1280 --depth 14 > logs/num_train_1280_depth_14.txt
---
+++ run5.sh
@@ -0,0 +1,4 @@
+python CNTK.py --num_train 10 --depth 5 > logs/num_train_10_depth_5.txt
+python CNTK.py --num_train 40 --depth 5 > logs/num_train_40_depth_5.txt
+python CNTK.py --num_train 160 --depth 5 > logs/num_train_160_depth_5.txt
+python CNTK.py --num_train 1280 --depth 5 > logs/num_train_1280_depth_5.txt
