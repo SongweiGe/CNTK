76b709471f80aa6185ad93e741d41220cbeef142
SG <gesongwei666@gmail.com>
2020-10-05 23:52:26 -0400

    scripts

---
+++ run23.sh
@@ -0,0 +1,4 @@
+python CNTK.py --num_train 10 --depth 23 > logs/num_train_10_depth_23.txt
+python CNTK.py --num_train 40 --depth 23 > logs/num_train_40_depth_23.txt
+python CNTK.py --num_train 160 --depth 23 > logs/num_train_160_depth_23.txt
+python CNTK.py --num_train 1280 --depth 23 > logs/num_train_1280_depth_23.txt
